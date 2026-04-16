2edfee60a71fad6c500d5f4f9eff94b4cefe035c
pankaj <pankajkumarsah940@gmail.com>
2026-04-17 03:54:45 +0530

    learned basic browser routing setup..

--- 14_router/src/App.jsx
+++ 14_router/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path= '/' element={<Home/>}/>
         <Route path= '/about' element={<About/>}/>
