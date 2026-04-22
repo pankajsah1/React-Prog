bd5a18fc1688b2d7a4c4e7d2c8558f5a9bb32d3a
pankaj <pankajkumarsah940@gmail.com>
2026-04-23 03:05:05 +0530

    19_React-Project -> set API one more to go

--- 19_React-Project/src/App.jsx
+++ 19_React-Project/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import { fetchPhoto } from './api/mediaApi'
+import { fetchGIF, fetchPhoto, fetchVideo } from './api/mediaApi'
 
 const App = () => {
 // console.log("KEY:", import.meta.env.VITE_UNSPLASH_KEY);
   return (
     <div className='h-screen w-full bg-gray-950 text-white'>
-      <button onClick={ async()=>{
+      <button className='bg-green-600 m-3 p-3' onClick={ async()=>{
         const data = await fetchPhoto('Cat')
-        console.log(data);
+        console.log(data.results);
         
       }}>Get Photos</button>
+
+      <button  className='bg-green-600 m-3 p-3' onClick={ async()=>{
+        const data = await fetchVideo('Cat')
+        console.log(data.videos);
+        
+      }}>Get Videos</button>
+
+      <button  className='bg-green-600 m-3 p-3' onClick={ async()=>{
+        const data = await fetchGIF('Cat')
+        console.log(data);
+        
+      }}>Get GIF</button>
     </div>
   )
 }
--- 19_React-Project/src/api/mediaApi.js
+++ 19_React-Project/src/api/mediaApi.js
@@ -2,11 +2,28 @@ import axios from 'axios'
 
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+const TENOR_KEY = import.meta.env.VITE_TENOR_KEY
 
 export async function fetchPhoto(query, page=1, per_page = 20){
    const response = await axios.get('https://api.unsplash.com/search/photos',{
     params:{query, page, per_page},
     headers: {Authorization: `Client-ID ${UNSPLASH_KEY}`}
+   })
+   return response.data
+}
+
+export async function fetchVideo(query, per_page=20) {
+  const response = await axios.get('https://api.pexels.com/v1/videos/search',{
+    params:{query, per_page},
+    headers: {Authorization:PEXELS_KEY}
+   })
+   return response.data
+}
+
+export async function fetchGIF(query,per_page=20) {
+  const response = await axios.get('https://api.giphy.com/v1/gifs/translate',{
+    params:{query, per_page},
+    headers:{Authorization: TENOR_KEY}
    })
    return response.data
 }
